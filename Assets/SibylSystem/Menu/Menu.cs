c3fece7f3210a5ce893e7d2d27e2bab56b69b088
jwyxym <jwyxym@126.com>
2024-08-12 15:42:29 +0800

    fix

--- Assets/SibylSystem/Menu/Menu.cs
+++ Assets/SibylSystem/Menu/Menu.cs
@@ -30,7 +30,7 @@ public class Menu : WindowServantSP
         UIHelper.registEvent(gameObject, "exit_", onClickExit);
         UIHelper.registEvent(gameObject, "animation_", onClickAnimation);
         //MDPRO2 update
-        UIHelper.getByName<UILabel>(gameObject, "version_").text = "2.1.3";
+        UIHelper.getByName<UILabel>(gameObject, "version_").text = "3.0.0";
         Program.I().StartCoroutine(checkUpdate());
     }
 
